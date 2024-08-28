0cd0e6413ce1513c8922ced056c20947d039c826
Mohini Borhade Thorat <borhade.mohini@gmail.com>
2024-08-28 14:34:41 +0530

    two sum prob

--- DSA/twoSum.js
+++ DSA/twoSum.js
@@ -1,16 +1,39 @@
-const twoSum = (arr, num) => {
-    let obj = {};
-    let res = [];
-    arr.map(item => {
-      let com = num - item; 
-      if (obj[com]) {
-        res.push([obj[com], item]);
-      } else {
-        obj[item] = item;
-      }
-    });
-    return res;
-  };
+// const twoSum = (arr, num) => {
+//     let obj = {};
+//     let res = [];
+//     arr.map(item => {
+//       let com = num - item; 
+//       if (obj[com]) {
+//         res.push([obj[com], item]);
+//       } else {
+//         obj[item] = item;
+//       }
+//     });
+//     return res;
+//   };
 
-  let result = twoSum([10, 15, 20, 30, 40, 25, 15], 40);
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+  let find;
+  let char = {};
+  let charIndex = {};
+  for (let i=0; i < nums.length; i++) {
+      let compliment = (target - nums[i]);
+      if (typeof char[compliment] !== 'undefined') {
+          find = [charIndex[compliment], i];
+          return find;
+      } else {
+          char[nums[i]] = nums[i];
+          charIndex[nums[i]] = i;
+      }
+      
+  }
+  return find;
+};
+
+  let result = twoSum([-3,4,3,90], 0);
   console.log("result :: ", result)
--- test.js
+++ test.js
@@ -1,1 +1,10 @@
 console.log("Test ");
+
+var removeDuplicates = function(nums) {
+    return [...new Set(nums)]; 
+ };
+
+let arr = [1, 1, 2]
+let result = removeDuplicates(arr);
+
+console.log("Result :: ", result);
